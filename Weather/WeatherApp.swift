d5ecf0e0657372b1c15644e775996e71bd6b2cf5
Isaí Arellano <isai.arellano@outlook.com>
2022-01-09 22:18:33 -0600

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Isaí on 09/01/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
